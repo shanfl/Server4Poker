5ec97e3c27bbb2d78f22e15b7df9db1e0f089389
shanfl <lyricc@sina.cn>
2024-05-14 20:54:37 +0800

    del

--- build/CMakeFiles/BaseServer.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for BaseServer.
-# This may be replaced when dependencies are built.
--- build/_deps/libuv-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/shanfl/KINGSTON/Server4Poker/build/_deps/libuv-src
-# Build directory: /media/shanfl/KINGSTON/Server4Poker/build/_deps/libuv-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
